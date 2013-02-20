518a0c829fccabb78b9533c07b74381dd54f0606
Andrei Varnavskiy <andrei.varnavskiy@cgtribe.com>
2013-02-20 17:18:22 +0400

    h1-h5 tags added in top level node list for ignoring in getNodesToDelete

--- goose/cleaners.py
+++ goose/cleaners.py
@@ -85,7 +85,7 @@ class DocumentCleaner(object):
             if node.tag in ['script','noscript','style','option'] or isinstance(node,lxml.html.HtmlComment):
 		nodelist.append(node)
 		continue
-            if node.tag in ['p','span','b'] and len(node) == 0: continue;
+            if node.tag in ['p','span','b','h1','h2','h3','h4','h5'] and len(node) == 0: continue;
             ids = ''
             if node.attrib.has_key('class'):
                ids += ' ' + node.attrib['class'].lower()
